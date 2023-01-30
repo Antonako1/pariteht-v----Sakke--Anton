34bb591793808bf7ab97f129ffb8e4062ffc7589
atlol <114558368+atlol@users.noreply.github.com>
2023-01-30 09:25:20 +0200

    aloitus

---
+++ JS - parityö/parityö.js
@@ -0,0 +1,36 @@
+
+
+
+// const person = {
+//     firstName: etunimi,
+//     lastName : sukunimi,
+//     id       : 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+// };
+// person.askName();
+
+
+// person.askName = function () {
+//     etunimi = prompt("etunimi");
+//     Object.defineProperty(person, "firstName", {writable: true})
+//     sukunimi = prompt("sukunimi")
+//     return etunimi, sukunimi;
+// }
+
+// function Person(first, last, age, eye) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+//   }
+  
+//   // Create two Person objects
+//   const myFather = new Person("John", "Doe", 50, "blue");
+//   const myMother = new Person("Sally", "Rally", 48, "green");
+  
+//   // Display age
+//   document.getElementById("demo").innerHTML =
+//   "My father is " + myFather.age + ". My mother is " + myMother.age + "."; 
+  
